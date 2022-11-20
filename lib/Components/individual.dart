14e0239508741474795d9cb253848108b849c635
Robert Perez <robertper280@gmail.com>
2022-11-20 00:12:21 -0800

    visual changes

--- lib/Components/individual.dart
+++ lib/Components/individual.dart
@@ -17,17 +17,23 @@ class Individual extends StatelessWidget {
       padding: const EdgeInsets.all(15.0),
       child: Container(
         padding: EdgeInsets.all(19),
-        decoration: BoxDecoration(color: Colors.lightBlue),
+        decoration: const BoxDecoration(color: Color(0xFF78281D)),
         child: Row(
           children: [
             Checkbox(
+              checkColor: const Color(0xFF78281D),
               value: completedTask,
               onChanged: onChanged,
-              activeColor: Colors.amber,
+              activeColor: Colors.white,
             ),
             Text(
               taskName,
               style: TextStyle(
+                  color: const Color(0xFFFFFFFF),
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20.0,
+                  letterSpacing: 2.0,
+                  fontFamily: 'Lato',
                   decoration: completedTask
                       ? TextDecoration.lineThrough
                       : TextDecoration.none),
